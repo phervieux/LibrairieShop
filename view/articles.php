060dbfbe19e393ee9969115db6e81fdf132206c0
root <root@lpsdebian.lopus>
2016-11-23 16:20:04 +0100

    TG-150 #closed bootstrap template

---
+++ view/articles.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>LibrairieShop</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous"> 
+    <link href="dist/theme/offcanvas.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+      <div class="container">
+        <a class="navbar-brand" href="">LibrairieShop</a>
+        <ul class="nav navbar-nav">
+          <li class="nav-item active"><a class="nav-link" href="">Magasin</a></li>
+          <li class="nav-item"><a class="nav-link" href="">Login/Enregistrer</a></li>
+        </ul>
+      </div><!-- /.container -->
+    </nav><!-- /.navbar -->
+
+    <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="float-xs-right hidden-sm-up">
+            <button type="button" class="btn btn-primary btn-sm" data-toggle="offcanvas">Mon panier</button>
+          </p>
+          <div class="jumbotron">
+            <form action="" method="POST" class="form-inline" role="form">
+              <input type="text" class="form-control" name="search" id="search" placeholder="Recherche de produits ...">
+              <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Rechercher</button>
+            </form>
+          </div>
+            <div class="row">
+              <div class="col-lg-4"><a class="thumbnail"><img src="dist/theme/img/book.jpeg" class="img-responsive img-product"></a></div>
+              <div class="col-lg-8">
+                <p>Donecssss id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <br><a class="btn btn-primary" href=""><i class="fa fa-shopping-cart"></i></a> <a class="btn btn-secondary" href=""><i class="fa fa-info"></i></a>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-lg-4"><a class="thumbnail"><img src="dist/theme/img/book.jpeg" class="img-responsive img-product"></a></div>
+              <div class="col-lg-8">
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <br><a class="btn btn-primary" href=""><i class="fa fa-shopping-cart"></i></a> <a class="btn btn-secondary" href=""><i class="fa fa-info"></i></a>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-lg-4"><a class="thumbnail"><img src="dist/theme/img/book.jpeg" class="img-responsive img-product"></a></div>
+              <div class="col-lg-8">
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <br><a class="btn btn-primary" href=""><i class="fa fa-shopping-cart"></i></a> <a class="btn btn-secondary" href=""><i class="fa fa-info"></i></a>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-lg-4"><a class="thumbnail"><img src="dist/theme/img/book.jpeg" class="img-responsive img-product"></a></div>
+              <div class="col-lg-8">
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <br><a class="btn btn-primary" href=""><i class="fa fa-shopping-cart"></i></a> <a class="btn btn-secondary" href=""><i class="fa fa-info"></i></a>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-lg-4"><a class="thumbnail"><img src="dist/theme/img/book.jpeg" class="img-responsive img-product"></a></div>
+              <div class="col-lg-8">
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <br><a class="btn btn-primary" href=""><i class="fa fa-shopping-cart"></i></a> <a class="btn btn-secondary" href=""><i class="fa fa-info"></i></a>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-lg-4"><a class="thumbnail"><img src="dist/theme/img/book.jpeg" class="img-responsive img-product"></a></div>
+              <div class="col-lg-8">
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <br><a class="btn btn-primary" href=""><i class="fa fa-shopping-cart"></i></a> <a class="btn btn-secondary" href=""><i class="fa fa-info"></i></a>
+              </div>
+            </div>
+            <hr>
+        </div><!--/span-->
+
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+          <div class="list-group">
+          <span class="list-group-item active"><h3>Mon panier</h3></span>
+            <span class="list-group-item">
+              Product<br><i>Quantité: 1</i><br>
+                <b class="pull-left">CHF 50.- </b><br>
+              <span class="pull-right">
+                <a href=""><i class="fa fa-trash-o"></i></a>
+                <a href=""><i class="fa fa-plus"></i></a>
+                <a href=""><i class="fa fa-minus"></i></a>
+              </span><br>
+            </span>
+            <span class="list-group-item">
+              Product<br><i>Quantité: 1</i><br>
+                <b class="pull-left">CHF 50.- </b><br>
+              <span class="pull-right">
+                <a href=""><i class="fa fa-trash-o"></i></a>
+                <a href=""><i class="fa fa-plus"></i></a>
+                <a href=""><i class="fa fa-minus"></i></a>
+              </span><br>
+            </span>
+            <span class="list-group-item">
+              Product<br><i>Quantité: 1</i><br>
+                <b class="pull-left">CHF 50.- </b><br>
+              <span class="pull-right">
+                <a href=""><i class="fa fa-trash-o"></i></a>
+                <a href=""><i class="fa fa-plus"></i></a>
+                <a href=""><i class="fa fa-minus"></i></a>
+              </span><br>
+            </span>
+            <span class="list-group-item">
+                <button type="button" class="btn btn-primary"><i class="fa fa-trash-o"></i></button>
+                <button type="button" class="btn btn-primary">Payer</button>
+            </span>
+          </div>
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p>&copy; LibrairieShop <?php echo date('Y'); ?></p>
+      </footer>
+
+    </div><!--/.container-->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="dist/theme/offcanvas.js"></script>
+  </body>
+</html>
