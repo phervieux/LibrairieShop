187145390df94bd88d812cc91a99a8003533f5b6
Migue Jalube <Migue Jalube>
2016-11-30 20:44:14 +0100

    add debug login

--- config/odbc_connection.php
+++ config/odbc_connection.php
@@ -2,9 +2,14 @@
 if (!defined('INCLUDE_CHECK')) {
     http_response_code(404); die;
 }
+$accessPath = $_SERVER['DOCUMENT_ROOT'].'/DATABASE/projet151.accdb';
+
+if (!file_exists($accessPath)) {
+    die("Access database file not found !");
+}
 
 //Establish the connection
-$odbc = new PDO('odbc:Driver={Microsoft Access Driver (*.mdb)};Dbq='.$_SERVER['DOCUMENT_ROOT'].'/DATABASE/projet151.accdb;Uid=');
+$odbc = new PDO('odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq='.$accessPath.';Uid=');
 
 //hide sensible information on PDO errors
 $odbc -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
